cf5ce1814d164e578cec5e7eeb7f1d1464c0b995
Nauman Haider <mhaidernauman@gmail.com>
2026-05-08 15:55:13 +0500

    update package.swift with new version

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DiscountWorldFramework",
-            url: "https://discount-world.sfo3.digitaloceanspaces.com/swift-package-manager/1.0.5/DiscountWorldFramework.xcframework.zip",
-            checksum: "050d25eb7320ff6a77834a4eb725b43cf23870cefb5ff702fe24774f8c969b98"
+            url: "https://discount-world.sfo3.digitaloceanspaces.com/swift-package-manager/1.0.6/DiscountWorldFramework.xcframework.zip",
+            checksum: "f1ae9c3462410a4c1d6bc6a419425d080b9fb28dfd454a10e28a4f01afebdeb5"
         )
     ]
 )
